ed0c33963ae8db18f0be897d160890a38e911f74
Jordan Skole <jordan@usebenchmark.com>
2018-08-10 16:55:01 -0500

    coming soon

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import Header from './components/Header'
 import Home from './components/Home'
 import Job from './components/Job'
+import ComingSoon from './components/ComingSoon'
 
 const Wrapper = styled.div`
   max-width: 800px;
@@ -17,7 +18,11 @@ const App = () => (
       <Header />
       <main>
         <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
+        <Route path={process.env.PUBLIC_URL + '/jobs'} component={ComingSoon} />
         <Route path={process.env.PUBLIC_URL + '/job/:id'} component={Job} />
+        <Route path={process.env.PUBLIC_URL + '/companies'} component={ComingSoon} />
+        <Route path={process.env.PUBLIC_URL + '/submit'} component={ComingSoon} />
+        <Route path={process.env.PUBLIC_URL + '/account'} component={ComingSoon} />
       </main>
     </Wrapper>
   </Router>
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1>Coming soon!🏗️</h1>
+      <p>
+        Wann help? <a href="https://github.com/jordanskole/detroit-startup-jobs/" target="_blank">Make a PR!</a>
+      </p>
+    </div>
+  )
+}
